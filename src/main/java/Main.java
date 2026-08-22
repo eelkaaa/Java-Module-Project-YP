a92875e9879794d5a5f915ad6e8143aae123f6ba
Elisei <elisei.mishchenko@yandex.ru>
2026-08-22 19:21:20 +0300

    Доработал логику по проверке пустого поля в названии машин. А также проверки пустого поля или неверных значений по типу "abc/#$%" или "2.50" в скорости машин.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < cars.length; i++) {
+            String name;
             System.out.println("Введите название машины №" + (i + 1));
-            String name = scanner.next();
-            System.out.println("Введите скорость машины №" + (i + 1));
             while (true) {
-                int speed = scanner.nextInt();
-                if (speed > 0 && speed <= 250) {
-                    cars[i] = new Car(name, speed);
-                    race.choosingLeader(cars[i]);
+                name = scanner.nextLine();
+                if (!name.isEmpty()) {
                     break;
                 } else {
-                    System.out.println("Введена неверная скорость, введите скорость от 0 до 250!");
+                    System.out.println("У каждой машины есть название! Тут не может быть пусто :(");
+                }
+            }
+
+            int speed;
+            System.out.println("Введите скорость машины №" + (i + 1));
+            while (true) {
+                // Столкнулся с проблемой проверки пустого ввода и значений "abc/#$%" или "2.50" в int
+                // Решение через try/catch подсказал ИИ, так как способы проверки вводимых данных через scanner на курсе ещё не проходили :)
+                try {
+                    speed = Integer.parseInt(scanner.nextLine());
+                    if (speed > 0 && speed <= 250) {
+                        cars[i] = new Car(name, speed);
+                        race.choosingLeader(cars[i]);
+                        break;
+                    } else {
+                        System.out.println("Введена неверная скорость, введите скорость от 0 до 250!");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Введите целочисленное число!");
                 }
             }
         }
